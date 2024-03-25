018357454891513614e4ee709269b0329c704686
wuyingge96@163.com <wuyingge96@163.com>
2024-03-26 02:44:04 +0800

    更新日志

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HuashengInterviewProblems
 answer for an interview
+
+
+前4题结果均在py文件中有所展示
+
+简要说下第五题的设计：
+
+采用fastapi + uvicorn实现异步并发实现。
+创建一个FastAPI应用，并加载xgboost模型。
+用sleep模拟耗时较长的预处理过程，使用asyncio来在后台执行这个任务，并在处理完成后将结果发送回客户端。
--- problem5/logger.py
+++ problem5/logger.py
@@ -1,12 +1,35 @@
-import logging
-
-
-LOG_PATH = "logs"
+import logging  
+import logging.handlers  
+import os  
+  
+# 日志文件目录  
+LOG_DIR = 'logs'  
+  
+# 确保日志文件目录存在  
+if not os.path.exists(LOG_DIR):  
+    os.makedirs(LOG_DIR)  
+  
+# 日志文件名  
+LOG_FILE = os.path.join(LOG_DIR, 'app.log')  
 
 def create_logger():
-    logger = logging.getLogger(__name__)
+    # 创建一个logger  
+    logger = logging.getLogger('myapp')  
+    logger.setLevel(logging.DEBUG)  
+    
+    # 创建一个handler，用于写入日志文件，并设置日志级别、日志格式等  
+    handler = logging.handlers.RotatingFileHandler(  
+        LOG_FILE, maxBytes=1024*1024, backupCount=5  # 日志文件最大1MB，保留5个备份  
+    )
+    
+    # 定义日志格式  
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  
+    handler.setFormatter(formatter)  
+    watch_handler = logging.handlers.WatchedFileHandler(LOG_FILE)  
+    
+    # 给logger添加handler  
+    logger.addHandler(handler)
+    logger.addHandler(watch_handler)
     return logger
-
-# logging.handlers.RotatingFileHandler()
 
 LOGGER = create_logger()
